4fe3c73894adde6efaa5de423486e94e85eeaf3d
vaivory <100865446+vaivory@users.noreply.github.com>
2022-08-30 21:25:03 +0300

    pirmi 5 el-tai

--- my-app/src/Components/006/Squares.jsx
+++ my-app/src/Components/006/Squares.jsx
@@ -42,9 +42,24 @@ function Squares() {
         setSq(s => s.map(square => square.number < 300 ? { ...square, show: false } : { ...square, show: true }));
     }
 
+    const [selected, SetSelected] = useState(null);
+
+    const doClick = num => {
+        SetSelected(num);
+        setSq(s => s.map(square => square.number === num ? { ...square, show: false } : { ...square }));
+    }
+
+    const undo = () => {
+        setSq(s => s.map(square => square.number === selected ? { ...square, show: true } : { ...square }));
+    }
+
+    const pirmElement = () => {
+        setSq(s => s.map((square, i) => i < 5 ? { ...square, show: true } : { ...square, show: false }));
+    }
+
     return (
         <>
-            <h1>STATE {sq.filter(s => s.number < 300).length}</h1>
+            <h1>{selected} STATE {sq.filter(s => s.number < 300).length}</h1>
             <div className="container">
                 {
                     sq.map((n, i) => n.show ? <div style={
@@ -52,7 +67,7 @@ function Squares() {
                             backgroundColor: (n.number < 300) ? 'black' : n.color,
                             borderRadius: (n.number % 2) ? null : '50px'
                         }
-                    } key={i}>{n.number}</div> : null)
+                    } key={i} onClick={() => doClick(n.number)}>{n.number}</div> : null)
                 }
             </div>
             <div className="container">
@@ -62,6 +77,8 @@ function Squares() {
                 <button onClick={sortDefault}>Sort default</button>
                 <button onClick={sortBlack}>Show only Black</button>
                 <button onClick={sortColored}>Show Colored</button>
+                <button onClick={undo}>Grazina paskutini istrinta el-ta</button>
+                <button onClick={pirmElement}>Pirmi 5 el-tai</button>
 
             </div>
         </>
